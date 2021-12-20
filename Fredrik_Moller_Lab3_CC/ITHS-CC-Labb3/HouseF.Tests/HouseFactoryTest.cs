d26212204f5815d89f34f77677e92b5581919ec3
Fredrik Möller <fredrik.moller@iths.se>
2021-12-20 10:02:09 +0100

    adds propertytests for factory

--- Fredrik_Moller_Lab3_CC/ITHS-CC-Labb3/HouseF.Tests/HouseFactoryTest.cs
+++ Fredrik_Moller_Lab3_CC/ITHS-CC-Labb3/HouseF.Tests/HouseFactoryTest.cs
@@ -42,6 +42,38 @@ namespace HouseF.Tests
             Assert.AreEqual(expectedBudgetHouse.HasGarage, ActualHouse.HasGarage);
             Assert.AreEqual(expectedBudgetHouse.HasSwimmingPool, ActualHouse.HasSwimmingPool);
         }
-        
+
+        [Test]
+        public void BuildHouse_GivenHouseIsEnviromentalHouse_ReturnExpectedHousePropertyValues()
+        {
+            EnviromentalHouse expectedEnviromentalHouse = new EnviromentalHouse();
+
+            var ActualHouse = _factory.BuildHouse("enviromentalHouse");
+
+            Assert.AreEqual(expectedEnviromentalHouse.StreetAdress, ActualHouse.StreetAdress);
+            Assert.AreEqual(expectedEnviromentalHouse.NoOfWindows, ActualHouse.NoOfWindows);
+            Assert.AreEqual(expectedEnviromentalHouse.ParkingSpotsInGarage, ActualHouse.ParkingSpotsInGarage);
+            Assert.AreEqual(expectedEnviromentalHouse.NoOfRooms, ActualHouse.NoOfRooms);
+            Assert.AreEqual(expectedEnviromentalHouse.HasGarage, ActualHouse.HasGarage);
+            Assert.AreEqual(expectedEnviromentalHouse.HasSwimmingPool, ActualHouse.HasSwimmingPool);
+        }
+
+        [Test]
+        public void BuildHouse_GivenHouseIsLuxaryMansionHouse_ReturnExpectedHousePropertyValues()
+        {
+            LuxaryMansionHouse expectedLuxaryMansionHouse = new LuxaryMansionHouse();
+
+            var ActualHouse = _factory.BuildHouse("luxarymansionhouse");
+
+            Assert.AreEqual(expectedLuxaryMansionHouse.StreetAdress, ActualHouse.StreetAdress);
+            Assert.AreEqual(expectedLuxaryMansionHouse.NoOfWindows, ActualHouse.NoOfWindows);
+            Assert.AreEqual(expectedLuxaryMansionHouse.ParkingSpotsInGarage, ActualHouse.ParkingSpotsInGarage);
+            Assert.AreEqual(expectedLuxaryMansionHouse.NoOfRooms, ActualHouse.NoOfRooms);
+            Assert.AreEqual(expectedLuxaryMansionHouse.HasGarage, ActualHouse.HasGarage);
+            Assert.AreEqual(expectedLuxaryMansionHouse.HasSwimmingPool, ActualHouse.HasSwimmingPool);
+        }
+
+
+
     }
 }
